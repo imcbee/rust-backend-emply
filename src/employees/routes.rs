6eee1b60720f89ebaf9fef7410a2f3d69fe09c54
Ian McBee <mcbee_ian@bah.com>
2023-07-28 13:51:28 -0400

    first push

---
+++ src/employees/routes.rs
@@ -0,0 +1,40 @@
+#[get("/employees")] //Get all employees
+async fn find_all() -> Result<HttpResponse, CustomError> {
+    let employees = web::block(|| Employees::find_all()).await.unwrap();
+    Ok(HttpResponse::Ok().json(employees))
+}
+
+#[get("/employees/{id}")] //Get single employee
+async fn find(id: web::Path) -> Result<HttpResponse, CustomError> {
+    let employee = Employees::find(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[post("/employees")] //Add employee
+async fn create(employee: web::Json) -> Result<HttpResponse, CustomError> {
+    let employee = Employees::create(employee.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[put("/employees/{id}")] //Update employee
+async fn update(
+    id: web::Path,
+    employee: web::Json,
+) -> Result<HttpResponse, CustomError> {
+    let employee = Employees::update(id.into_inner(), employee.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[delete("/employees/{id}")] //Delete employee
+async fn delete(id: web::Path) -> Result<HttpResponse, CustomError> {
+    let deleted_employee = Employees::delete(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_employee })))
+}
+
+pub fn init_routes(config: &mut web::ServiceConfig) {
+    config.service(find_all);
+    config.service(find);
+    config.service(create);
+    config.service(update);
+    config.service(delete);
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok(); // Helps to manage our environment variables
+    db::init(); // A function that initiates the database connection, which is referenced from another file called db.rs
+    let mut listenfd = ListenFd::from_env(); // Restarts the server when changes is detected in files
+    let mut server = HttpServer::new(|| App::new().configure(employees::init_routes)); // A function that abstracts all routes used in this project
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Please set host in .env");
+            let port = env::var("PORT").expect("Please set port in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+    server.run().await
+}
