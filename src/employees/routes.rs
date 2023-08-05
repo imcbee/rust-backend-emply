ea776ae0479579ac380e8f6e7d5d171f61818df9
Ian McBee <mcbee_ian@bah.com>
2023-08-05 14:08:07 -0400

    install new dependecies and debugging build

--- src/employees/routes.rs
+++ src/employees/routes.rs
@@ -3,43 +3,43 @@ use crate::error_handler::CustomError;
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
 
-#[get("/employees")] //Get all employees
+#[get("/employees")]
 async fn find_all() -> Result<HttpResponse, CustomError> {
-    let employees = web::block(|| Employees::find_all()).await.unwrap();
+    let employees = Employees::find_all()?;
     Ok(HttpResponse::Ok().json(employees))
 }
 
-#[get("/employees/{id}")] //Get single employee
-async fn find(id: web::Path) -> Result<HttpResponse, CustomError> {
+#[get("/employees/{id}")]
+async fn find(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
     let employee = Employees::find(id.into_inner())?;
     Ok(HttpResponse::Ok().json(employee))
 }
 
-#[post("/employees")] //Add employee
-async fn create(employee: web::Json) -> Result<HttpResponse, CustomError> {
+#[post("/employees")]
+async fn create(employee: web::Json<Employee>) -> Result<HttpResponse, CustomError> {
     let employee = Employees::create(employee.into_inner())?;
     Ok(HttpResponse::Ok().json(employee))
 }
 
-#[put("/employees/{id}")] //Update employee
+#[put("/employees/{id}")]
 async fn update(
-    id: web::Path,
-    employee: web::Json,
+    id: web::Path<i32>,
+    employee: web::Json<Employee>,
 ) -> Result<HttpResponse, CustomError> {
     let employee = Employees::update(id.into_inner(), employee.into_inner())?;
     Ok(HttpResponse::Ok().json(employee))
 }
 
-#[delete("/employees/{id}")] //Delete employee
-async fn delete(id: web::Path) -> Result<HttpResponse, CustomError> {
+#[delete("/employees/{id}")]
+async fn delete(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
     let deleted_employee = Employees::delete(id.into_inner())?;
     Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_employee })))
 }
 
-pub fn init_routes(config: &mut web::ServiceConfig) {
-    config.service(find_all);
-    config.service(find);
-    config.service(create);
-    config.service(update);
-    config.service(delete);
+pub fn init_routes(comfig: &mut web::ServiceConfig) {
+    comfig.service(find_all);
+    comfig.service(find);
+    comfig.service(create);
+    comfig.service(update);
+    comfig.service(delete);
 }
